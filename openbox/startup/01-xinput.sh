0cc22b95063141aa52e26a149c961ec916565cb7
sanvew <sanvew@protonmail.com>
2025-08-10 23:52:41 +0200

    polybar: new modules, xterm: theme: - polybar: add modules: battery, memory, cpu - polybar: module refactoring - xterm: dark theme

--- openbox/startup/01-xinput.sh
+++ openbox/startup/01-xinput.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TOUCHPAD_ID=$(xinput list | grep -i touchpad | grep -o 'id=[0-9]\+' | cut -d= -f2)
-if [ -n "$TOUCHPAD"]; then
+if [ -n "$TOUCHPAD_ID" ]; then
     xinput set-prop "$TOUCHPAD_ID" "libinput Tapping Enabled" 1
     xinput set-prop "$TOUCHPAD_ID" "libinput Tapping Drag Enabled" 1
     xinput set-prop "$TOUCHPAD_ID" "libinput Tapping Drag Lock Enabled" 1
