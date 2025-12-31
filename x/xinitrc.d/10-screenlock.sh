1332df41f346485cda4496377f7e0b4f70730406
sanvew <sanvew@protonmail.com>
2025-12-31 15:12:18 +0100

    adjust screen lock and hibernate timeouts

--- x/xinitrc.d/10-screenlock.sh
+++ x/xinitrc.d/10-screenlock.sh
@@ -3,8 +3,10 @@
 killall -q xidlehook
 
 LOCK_TIMEOUT=$(echo "5*60" | bc)
+HIBERNATE_TIMEOUT=$(echo "25*60" | bc)
 SCREEN_LOCKER_CMD=slock
 
 [ -x "$(command -v xidlehook)" ] \
     && xidlehook --not-when-fullscreen --not-when-audio \
-        --timer $LOCK_TIMEOUT "$SCREEN_LOCKER_CMD" ""
+        --timer $LOCK_TIMEOUT "$SCREEN_LOCKER_CMD" "" \
+        --timer $HIBERNATE_TIMEOUT "loginctl hibernate" ""
