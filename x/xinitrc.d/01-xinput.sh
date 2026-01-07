47a18330f48ef6236752e8fd9ab59beb46d5c7a5
sanvew <sanvew@protonmail.com>
2026-01-07 22:56:07 +0100

    move xinput settings to persistant location - move xinput startup file content to Xorg config dir - add root priviliged modules

--- x/xinitrc.d/01-xinput.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-TOUCHPAD_ID=$(xinput list | grep -i touchpad | grep -o 'id=[0-9]\+' | cut -d= -f2)
-if [ -n "$TOUCHPAD_ID" ]; then
-    xinput set-prop "$TOUCHPAD_ID" "libinput Tapping Enabled" 1
-    xinput set-prop "$TOUCHPAD_ID" "libinput Tapping Drag Enabled" 1
-    xinput set-prop "$TOUCHPAD_ID" "libinput Tapping Drag Lock Enabled" 1
-    xinput set-prop "$TOUCHPAD_ID" "libinput Natural Scrolling Enabled" 1
-fi
