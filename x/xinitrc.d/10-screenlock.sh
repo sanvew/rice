677afbcd1f3efc06d990df3f3a0f288ac10f8c35
sanvew <sanvew@protonmail.com>
2026-01-11 20:23:51 +0100

    add dmenu "shutdown" script - rework of screenlock

--- x/xinitrc.d/10-screenlock.sh
+++ x/xinitrc.d/10-screenlock.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-killall -q xidlehook
-
-LOCK_TIMEOUT=$(echo "5*60" | bc)
-HIBERNATE_TIMEOUT=$(echo "25*60" | bc)
-SCREEN_LOCKER_CMD=slock
+LOCK_TIMEOUT=5
+SCREENLOCK_SRIPT="screenlock"
 
 [ -x "$(command -v xidlehook)" ] \
     && xidlehook --not-when-fullscreen --not-when-audio \
-        --timer $LOCK_TIMEOUT "$SCREEN_LOCKER_CMD" "" \
-        --timer $HIBERNATE_TIMEOUT "loginctl hibernate" ""
+        --timer "$(echo "$LOCK_TIMEOUT*60" | bc)" "$SCREENLOCK_SRIPT" ""
